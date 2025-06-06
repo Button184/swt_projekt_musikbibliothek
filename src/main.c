f4c131a0681203396fa15e30cf92194007ac3cd9
Eric Braun <erbrmt00@hs-esslingen.de>
2025-06-06 14:25:04 +0200

    Test

--- src/main.c
+++ src/main.c
@@ -1,43 +1,55 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_LAENGE 50
+
+typedef struct {
+    char titel [MAX_LAENGE];
+    char interpret [MAX_LAENGE];
+    char album [MAX_LAENGE];
+    int lieddauer;
+    int erscheinungsjahr;
+} Lied;
 
 void BibliothekErstellen() {
-
 }
 
 void BibliothekAnzeigen() {
-
 }
 
 void BibliothekenAusgeben() {
-
 }
 
 void LiedHinzufuegen() {
-
 }
 
 void MetaDatenAendern() {
-
 }
 
 void LiedLoeschen() {
-
 }
 
 void MetaDatenSuchen() {
-
 }
 
 void BibliothekLoeschen() {
-
 }
 
 void Speichern() {
-
 }
 
 int main() {
+
+    int auswahl;
+
+        printf("---------------------------------\n");
+        printf("Willkommen in der Musikbibliothek\n");
+        printf("---------------------------------\n");
+        printf("\n");
+        printf("Wähle eine Aktion aus:\n");
+
+        // Aktionen einbauen
 
     return 0;
 }
