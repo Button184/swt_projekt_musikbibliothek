2f43354c73246612163a8b44c15731dae8fc17c6
Eric Braun <erbrmt00@hs-esslingen.de>
2025-06-06 13:51:35 +0200

    Branch für voids

--- src/main.c
+++ src/main.c
@@ -1,6 +1,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void BibliothekErstellen() {
+
+}
+
+void BibliothekAnzeigen() {
+
+}
+
+void BibliothekenAusgeben() {
+
+}
+
+void LiedHinzufuegen() {
+
+}
+
+void MetaDatenAendern() {
+
+}
+
+void LiedLoeschen() {
+
+}
+
+void MetaDatenSuchen() {
+
+}
+
+void BibliothekLoeschen() {
+
+}
+
+void Speichern() {
+
+}
 
 int main() {
 
